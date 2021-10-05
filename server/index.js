80cad995ac1d1aa995e35e4f784824e2ff4e8d47
Elijah Robinson <47159753+erobin27@users.noreply.github.com>
2021-10-05 06:53:39 -0400

    Added new API calls and cleaned console up

--- server/index.js
+++ server/index.js
@@ -13,25 +13,84 @@ app.use(function(req, res, next){
     next();
 });
 
-app.get('/nextEvent', async (req, res) => {
-    const fightData = await scrapers.scrapeNextEvent('https://www.ufc.com/events')
+app.get('/api/v1/nextEvent', async (req, res) => {
+    console.log('API request at: /nextEvent')
+
+    try{
+        const fightData = await scrapers.scrapeNextEvent('https://www.ufc.com/events')
+    }
+    catch(err){
+        console.error(err)
+        res.send({'Error' : 'Your specified url is not contained. Try using the /eventLinks feature to see available events'})
+    }
     res.send(fightData)
+    console.log('Success')
     //todo: get ufc odds
 });
 
-app.get('/eventLinks', async (req, res) => {
-    const eventLinks = await scrapers.scrapeEventUrls('https://www.ufc.com/events')
+app.get('/api/v1/eventLinks', async (req, res) => {
+    console.log('API request at: /eventLinks')
+
+    try{
+        const eventLinks = await scrapers.scrapeEventUrls('https://www.ufc.com/events')
+    }
+    catch(err) {
+        console.error(err)
+        res.send({'Error' : 'Your specified url is not contained. Try using the /eventLinks feature to see available events'})
+    }
+
     res.send(eventLinks)
+    console.log('Success')
     //todo: get ufc odds
 });
 
-app.get('/eventByLink', async (req, res) => {
+app.get('/api/v1/allEvents', async (req, res) => { //SLOW
     //const eventLinks = await scrapers.scrapeEventUrls(req)
-    res.send(eventLinks)
+    console.log('API request at: /allEvents')
+
+    try{
+        const allEvents = await scrapers.scrapeAllUpcomingEvents('https://www.ufc.com/events')
+    }
+    catch(err){
+        console.error(err)
+        res.send({'Error' : 'Your specified url is not contained. Try using the /eventLinks feature to see available events'})
+    }
+
+
+    res.send(allEvents)
+    console.log('Success')
+});
+
+app.get('/api/v1/eventByLink', async (req, res) => { //SLOW
+    //const eventLinks = await scrapers.scrapeEventUrls(req)
+    console.log('API request at: eventByLink')
+    try{
+        req.query.url;
+    }  
+    catch(err) {
+        console.error('EventByLink had no url field in query')
+        res.send({'Error' : 'Format your query like this `/eventByLink?url=https://www.ufc.com/event/ufc-268` you are recieving an error for no url query'})
+    }
+
+    try{
+        if(req.query.url.toLowerCase().indexOf('https://www.ufc.com/event/') == -1) 
+        {
+            console.error('EventLink was not a ufc link')
+            throw 'not ufc link';
+        }
+
+        const event = await scrapers.scrapeByUrl(req.query.url)
+        res.send(event)
+        console.log('Success')
+    }
+    catch(err){
+        console.error(err)
+        res.send({'Error' : 'Your specified url is not contained. Try using the /eventLinks feature to see available events'})
+    }
     //todo: get ufc odds
 });
 
-app.post('/fight', async (req, res) => {
+app.post('/api/v1/fight', async (req, res) => {
     console.log(req.body)
     //const fightData = await scrapers.scrapePage('https://sportsbook.draftkings.com/leagues/mma/88670562?category=fight-lines&subcategory=moneyline')
     //console.log({fightData})
@@ -40,5 +99,25 @@ app.post('/fight', async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`UFC app listening at http://localhost:${port}`)
+
+    console.logCopy = console.log.bind(console);
+
+    console.log = function(data)
+    {
+        var currentDate = '[' + new Date().toLocaleString() + '] ';
+        let colorStart = '\u001b[' + 32 + 'm';
+        let colorEnd = '\u001b[0m'
+        this.logCopy(colorStart, currentDate, colorEnd, data);
+    };
+
+    console.error = function(data)
+    {
+        var currentDate = '[' + new Date().toLocaleString() + '] ';
+        let colorStart = '\u001b[' + 31 + 'm';
+        let colorEnd = '\u001b[0m'
+        this.logCopy(colorStart, currentDate, colorEnd, data);
+    };
+
+    console.log('Server Started.');
 });
--- server/scrapers.js
+++ server/scrapers.js
@@ -44,7 +44,7 @@ async function scrapeNextEvent(url) {
         let obj = {};
         obj['eventTitle'] = window.location.pathname.split('/').pop();
         obj['date'] = document.getElementsByClassName('c-event-fight-card-broadcaster__time tz-change-inner')[0].textContent;
-        obj['name'] = document.getElementsByClassName('field field--name-venue field--type-entity-reference field--label-hidden field__item')[0].textContent;
+        //obj['name'] = document.getElementsByClassName('field field--name-venue field--type-entity-reference field--label-hidden field__item')[0].textContent;
         obj['fights'] = data;
         return obj;
     });
@@ -52,6 +52,75 @@ async function scrapeNextEvent(url) {
     browser.close()
     return fightInfo    //Return JSON object with event info and all fights on the event
 }
+
+async function scrapeAllUpcomingEvents(url) {
+    //Launch Puppeteer and navigate to URL
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url)
+
+
+    //Get each event element and store the link to the event pages in eventLinks array
+    let eventLinks = await page.evaluate(async () => {
+        let data = [];
+        let events = document.getElementsByClassName('c-card-event--result__headline')
+        for (var event of events)
+            data.push(event.childNodes[0].href);
+        
+        return data;
+        //eventLink = events[0].childNodes[0].href;
+    });
+
+
+    //Navigate to next event page
+
+    var ufcEvents = {};
+
+    for(let i = 0; i < eventLinks.length; i++){
+
+        await page.goto(eventLinks[i])
+        let fightInfo = await page.evaluate(async () => {
+            let data = [];
+
+            //Store a list of all fighters first and last names
+            let givenNames = document.getElementsByClassName('c-listing-fight__corner-given-name');
+            let lastNames = document.getElementsByClassName('c-listing-fight__corner-family-name');
+            let fighterOdds = document.getElementsByClassName('c-listing-fight__odds-amount');
+
+            //Loop through all fights and create an array with objects including their names and odds
+            for(let i = 0; i < givenNames.length-1; i+=2){
+                fightTitle = lastNames[i].textContent + ' vs ' + lastNames[i+1].textContent
+                let obj2 = {};
+                obj2['Red'] =  {'Name': givenNames[i].textContent + ' ' +lastNames[i].textContent, 'Odds': fighterOdds[i].textContent};
+                obj2['Blue'] = {'Name': givenNames[i+1].textContent + ' ' +lastNames[i+1].textContent, 'Odds': fighterOdds[i+1].textContent};
+
+                let fightData = {}
+                fightData[fightTitle] = obj2
+                data.push(fightData);  
+            }
+
+            let obj = {};
+
+
+            /*
+            *
+            *   THERE NEEDS TO BE A TRY CATCH UNDER THIS
+            *   I have commented out the location scraper because if the event is too far in the future it will cause an issue when location isn't determined
+            */
+
+            obj['eventTitle'] = window.location.pathname.split('/').pop();
+            obj['date'] = document.getElementsByClassName('c-hero__headline-suffix tz-change-inner')[0].textContent;
+            //obj['location'] = document.getElementsByClassName('field field--name-venue field--type-entity-reference field--label-hidden field__item')[0].textContent;
+            obj['fights'] = data;
+            return [obj,window.location.href];
+        });
+        //URL: FightInfo ............. you can search for the event by url in the api
+        ufcEvents[fightInfo[1]] = fightInfo[0]    
+    }
+    browser.close()
+    return ufcEvents   //Return JSON object with event info and all Events
+}
+
 
 async function scrapeEventUrls(url) {
     //Launch Puppeteer and navigate to URL
@@ -74,12 +143,54 @@ async function scrapeEventUrls(url) {
     return eventLinks;
 }
 
+async function scrapeByUrl(url) {
+    //Launch Puppeteer and navigate to URL
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url)
+
+    let fightInfo = await page.evaluate(async () => {
+        let data = [];
+
+        //Store a list of all fighters first and last names
+        let givenNames = document.getElementsByClassName('c-listing-fight__corner-given-name');
+        let lastNames = document.getElementsByClassName('c-listing-fight__corner-family-name');
+        let fighterOdds = document.getElementsByClassName('c-listing-fight__odds-amount');
+
+        //Loop through all fights and create an array with objects including their names and odds
+        for(let i = 0; i < givenNames.length-1; i+=2){
+            fightTitle = lastNames[i].textContent + ' vs ' + lastNames[i+1].textContent
+            let obj2 = {};
+            obj2['Red'] =  {'Name': givenNames[i].textContent + ' ' +lastNames[i].textContent, 'Odds': fighterOdds[i].textContent};
+            obj2['Blue'] = {'Name': givenNames[i+1].textContent + ' ' +lastNames[i+1].textContent, 'Odds': fighterOdds[i+1].textContent};
+
+            let fightData = {}
+            fightData[fightTitle] = obj2
+            data.push(fightData);  
+        }
+
+        let obj = {};
+        obj['eventTitle'] = window.location.pathname.split('/').pop();
+        obj['date'] = document.getElementsByClassName('c-event-fight-card-broadcaster__time tz-change-inner')[0].textContent;
+        //obj['name'] = document.getElementsByClassName('field field--name-venue field--type-entity-reference field--label-hidden field__item')[0].textContent;
+        obj['fights'] = data;
+        return obj;
+    });
+
+    browser.close()
+    return fightInfo    //Return JSON object with event info and all fights on the event
+}
+
+
+
 (async () => {
-    //const data = await scrapePage('https://www.ufc.com/events')
+    //const data = await scrapeAllUpcomingEvents('https://www.ufc.com/events')
     //console.log(data)
 })();
 
 module.exports = {
     scrapeNextEvent,
-    scrapeEventUrls
+    scrapeEventUrls,
+    scrapeAllUpcomingEvents,
+    scrapeByUrl
 }
