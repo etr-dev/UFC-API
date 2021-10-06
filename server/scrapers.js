e98c851321af01927105ed920d96534df82140f1
erobin27 <elijah1998robinson@gmail.com>
2021-10-06 03:42:53 -0400

    added outcome methods to api response for byURL

--- server/scrapers.js
+++ server/scrapers.js
@@ -130,7 +130,6 @@ async function scrapeAllUpcomingEvents(url) {
     return ufcEvents   //Return JSON object with event info and all Events
 }
 
-
 async function scrapeEventUrls(url) {
     //Launch Puppeteer and navigate to URL
     const browser = await puppeteer.launch();
@@ -170,54 +169,16 @@ async function scrapeByUrl(url) {
         for (let res of outcomeList){
             outcome.push(res.childNodes[1].textContent)
         }
+        let method = document.getElementsByClassName('c-listing-fight__result-text method');
+        let time = document.getElementsByClassName('c-listing-fight__result-text time');
+        let round = document.getElementsByClassName('c-listing-fight__result-text round');
 
         //Loop through all fights and create an array with objects including their names and odds
         for(let i = 0; i < givenNames.length-1; i+=2){
             fightTitle = lastNames[i].textContent + ' vs ' + lastNames[i+1].textContent
+
             let obj2 = {};
-            obj2['Red'] =  {'Name': givenNames[i].textContent + ' ' +lastNames[i].textContent, 'Odds': fighterOdds[i].textContent, 'Outcome' : outcome[i].toLowerCase().replace(/(\r\n|\n|\r)/gm, "").trim()};
-            obj2['Blue'] = {'Name': givenNames[i+1].textContent + ' ' +lastNames[i+1].textContent, 'Odds': fighterOdds[i+1].textContent, 'Outcome': outcome[i+1].toLowerCase().replace(/(\r\n|\n|\r)/gm, "").trim()};
-
-            let fightData = {}
-            fightData[fightTitle] = obj2
-            data.push(fightData);  
-        }
-
-        let obj = {};
-        obj['eventTitle'] = window.location.pathname.split('/').pop();
-        obj['date'] = document.getElementsByClassName('c-event-fight-card-broadcaster__time tz-change-inner')[0].textContent;
-        //obj['name'] = document.getElementsByClassName('field field--name-venue field--type-entity-reference field--label-hidden field__item')[0].textContent;
-        obj['fights'] = data;
-        return obj;
-    });
-
-    browser.close()
-    return fightInfo    //Return JSON object with event info and all fights on the event
-}
-
-async function scrapeWinners(url) {
-    //Launch Puppeteer and navigate to URL
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(url)
-
-    let fightInfo = await page.evaluate(async () => {
-        let data = [];
-
-        //Store a list of all fighters first and last names
-        let givenNames = document.getElementsByClassName('c-listing-fight__corner-given-name');
-        let lastNames = document.getElementsByClassName('c-listing-fight__corner-family-name');
-        let fighterOdds = document.getElementsByClassName('c-listing-fight__odds-amount');
-        let outcomeList = document.getElementsByClassName('c-listing-fight__outcome-wrapper  ');
-        let outcome = [];
-        for (let res of outcomeList){
-            outcome.push(res.childNodes[1].textContent)
-        }
-
-        //Loop through all fights and create an array with objects including their names and odds
-        for(let i = 0; i < givenNames.length-1; i+=2){
-            fightTitle = lastNames[i].textContent + ' vs ' + lastNames[i+1].textContent
-            let obj2 = {};
+            obj2['OutcomeInfo'] = {'method': method[i/2].textContent, 'time': time[i/2].textContent,'round': round[i/2].textContent};
             obj2['Red'] =  {'Name': givenNames[i].textContent + ' ' +lastNames[i].textContent, 'Odds': fighterOdds[i].textContent, 'Outcome' : outcome[i].toLowerCase().replace(/(\r\n|\n|\r)/gm, "").trim()};
             obj2['Blue'] = {'Name': givenNames[i+1].textContent + ' ' +lastNames[i+1].textContent, 'Odds': fighterOdds[i+1].textContent, 'Outcome': outcome[i+1].toLowerCase().replace(/(\r\n|\n|\r)/gm, "").trim()};
 
@@ -239,7 +200,7 @@ async function scrapeWinners(url) {
 }
 
 (async () => {
-    //const data = await scrapeWinners('https://www.ufc.com/event/ufc-fight-night-october-09-2021')
+    //const data = await scrapeByUrl('https://www.ufc.com/event/ufc-fight-night-october-09-2021')
     //console.log(data)
 })();
 
@@ -247,6 +208,5 @@ module.exports = {
     scrapeNextEvent,
     scrapeEventUrls,
     scrapeAllUpcomingEvents,
-    scrapeByUrl,
-    scrapeWinners
+    scrapeByUrl
 }
