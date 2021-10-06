0069875567083b938604fa06be8b12ae2826af94
erobin27 <elijah1998robinson@gmail.com>
2021-10-06 02:20:01 -0400

    Added 15min caching for api

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyparser = require('body-parser');
+const NodeCache = require("node-cache");
+const cache = new NodeCache({ stdTTL: 900 });
 const colors = require('colors');
 const app = express();
 const scrapers = require("./scrapers");
+const e = require('express');
 const port = 3000;
 
 
@@ -17,48 +20,67 @@ app.use(function(req, res, next){
 
 app.get('/api/v1/nextEvent', async (req, res) => {
     console.log(colors.green('[' + new Date().toLocaleString() + ']    ' )+ 'API request at: /nextEvent')
-
+    if (cache.has('nextEvent')){
+        console.log(colors.green('[' + new Date().toLocaleString() + ']    ' )+'Cache hit for nextEvent')
+        return res.send(cache.get('nextEvent'));
+    } else {
     try{
+        console.log(colors.green('[' + new Date().toLocaleString() + ']    ' )+ 'Scraping...')
         const fightData = await scrapers.scrapeNextEvent('https://www.ufc.com/events')
+        cache.set('nextEvent', fightData)
         res.send(fightData)
     }
     catch(err){
         console.error(colors.red('[' + new Date().toLocaleString() + ']    ' )+err)
         res.send({'Error' : 'Your specified url is not contained. Try using the /eventLinks feature to see available events'})
     }
-    res.send(fightData)
     console.log(colors.green('[' + new Date().toLocaleString() + ']    ' )+'Success')
     //todo: get ufc odds
+    }
 });
 
 app.get('/api/v1/eventLinks', async (req, res) => {
     console.log(colors.green('[' + new Date().toLocaleString() + ']    ' )+'API request at: /eventLinks')
-
-    try{
-        const eventLinks = await scrapers.scrapeEventUrls('https://www.ufc.com/events')
-        res.send(eventLinks)
+    if (cache.has('eventLinks')){
+        console.log(colors.green('[' + new Date().toLocaleString() + ']    ' )+'Cache hit for eventLinks')
+        return res.send(cache.get('eventLinks'));
+    } else {
+        console.log(colors.green('[' + new Date().toLocaleString() + ']    ' )+'Scraping...')
+        try{
+            const eventLinks = await scrapers.scrapeEventUrls('https://www.ufc.com/events')
+            cache.set("eventLinks",eventLinks)
+            res.send(eventLinks)
+        }
+        catch(err) {
+            console.error(colors.red('[' + new Date().toLocaleString() + ']    ' )+err)
+            cache.
+            res.send({'Error' : 'Your specified url is not contained. Try using the /eventLinks feature to see available events'})
+        }
+        console.log(colors.green('[' + new Date().toLocaleString() + ']    ' )+'Success')
+        //todo: get ufc odds
     }
-    catch(err) {
-        console.error(colors.red('[' + new Date().toLocaleString() + ']    ' )+err)
-        res.send({'Error' : 'Your specified url is not contained. Try using the /eventLinks feature to see available events'})
-    }
-    console.log(colors.green('[' + new Date().toLocaleString() + ']    ' )+'Success')
-    //todo: get ufc odds
 });
 
 app.get('/api/v1/allEvents', async (req, res) => { //SLOW
     //const eventLinks = await scrapers.scrapeEventUrls(req)
     console.log(colors.green('[' + new Date().toLocaleString() + ']    ' )+'API request at: /allEvents')
 
-    try{
-        const allEvents = await scrapers.scrapeAllUpcomingEvents('https://www.ufc.com/events')
-        res.send(allEvents)
+    if (cache.has('allEvents')){
+        console.log(colors.green('[' + new Date().toLocaleString() + ']    ' )+'Cache hit for allEvents')
+        return res.send(cache.get('allEvents'));
+    } else {
+        console.log(colors.green('[' + new Date().toLocaleString() + ']    ' )+'Scraping...')
+        try{
+            const allEvents = await scrapers.scrapeAllUpcomingEvents('https://www.ufc.com/events')
+            cache.set("allEvents",allEvents)
+            res.send(allEvents)
+        }
+        catch(err){
+            console.error(colors.red('[' + new Date().toLocaleString() + ']    ' )+err)
+            res.send({'Error' : 'Your specified url is not contained. Try using the /eventLinks feature to see available events'})
+        }
+        console.log(colors.green('[' + new Date().toLocaleString() + ']    ' )+'Success')
     }
-    catch(err){
-        console.error(colors.red('[' + new Date().toLocaleString() + ']    ' )+err)
-        res.send({'Error' : 'Your specified url is not contained. Try using the /eventLinks feature to see available events'})
-    }
-    console.log(colors.green('[' + new Date().toLocaleString() + ']    ' )+'Success')
 });
 
 app.get('/api/v1/eventByLink', async (req, res) => { //SLOW
@@ -72,20 +94,27 @@ app.get('/api/v1/eventByLink', async (req, res) => { //SLOW
         res.send({'Error' : 'Format your query like this `/eventByLink?url=https://www.ufc.com/event/ufc-268` you are recieving an error for no url query'})
     }
 
-    try{
-        if(req.query.url.toLowerCase().indexOf('https://www.ufc.com/event/') == -1) 
-        {
-            console.error(colors.red('[' + new Date().toLocaleString() + ']    ' )+'EventLink was not a ufc link')
-            throw 'not ufc link';
-        }
+    if(cache.has(req.query.url.toLowerCase())){
+        console.log(colors.green('[' + new Date().toLocaleString() + ']    ' )+'Cache hit for ' + req.query.url)
+        return res.send(cache.get(req.query.url))
+    } else {
+        console.log(colors.green('[' + new Date().toLocaleString() + ']    ' )+'Scraping...')
+        try{
+            if(req.query.url.toLowerCase().indexOf('https://www.ufc.com/event/') == -1) 
+            {
+                console.error(colors.red('[' + new Date().toLocaleString() + ']    ' )+'EventLink was not a ufc link')
+                throw 'not ufc link';
+            }
 
-        const event = await scrapers.scrapeByUrl(req.query.url)
-        res.send(event)
-        console.log(colors.green('[' + new Date().toLocaleString() + ']    ' )+'Success')
-    }
-    catch(err){
-        console.error(colors.red('[' + new Date().toLocaleString() + ']    ' )+err)
-        res.send({'Error' : 'Your specified url is not contained. Try using the /eventLinks feature to see available events'})
+            const event = await scrapers.scrapeByUrl(req.query.url)
+            cache.set(req.query.url.toLowerCase(),event)      //Store the URL data in the Cache
+            res.send(event)
+            console.log(colors.green('[' + new Date().toLocaleString() + ']    ' )+'Success')
+        }
+        catch(err){
+            console.error(colors.red('[' + new Date().toLocaleString() + ']    ' )+err)
+            res.send({'Error' : 'Your specified url is not contained. Try using the /eventLinks feature to see available events'})
+        }
     }
     //todo: get ufc odds
 });
